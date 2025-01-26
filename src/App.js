dae14e846dd14d848aba66881c77eca5dcf135d0
Harsh Setia <setia.harsh@gmail.com>
2025-01-26 14:28:11 +0000

    amplify

--- src/App.js
+++ src/App.js
@@ -9,7 +9,11 @@ import CandidateHire from "./pages/CandidateHire";
 import ContactUs from "./pages/ContactUs";
 import CompanyName from './CompanyName';
 import "./App.css";
-
+import '@aws-amplify/ui-react/styles.css';
+import '@aws-amplify/ui-react/styles/reset.layer.css' // global CSS reset
+import '@aws-amplify/ui-react/styles/base.layer.css' // base styling needed for Amplify UI
+import '@aws-amplify/ui-react/styles/button.layer.css' // component specific styles
+import '@fontsource/inter/variable.css';
 
 function App() {
     return (
